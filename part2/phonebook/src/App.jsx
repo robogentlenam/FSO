c0c517a78fa0eec11a93bd86b7cc4783a933d799
robogentlenam <121732750+robogentlenam@users.noreply.github.com>
2025-01-03 14:03:07 +0100

    2.9*: The Phonebook Step 4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,6 +4,7 @@ const App = () => {
   const [persons, setPersons] = useState([{ id: "1", name: "Arto Hellas", number:"111 111 111"}]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("")
+  const [filterName, setFilterName] =useState("")
 
   const addName = (event) => {
     event.preventDefault();
@@ -28,6 +29,10 @@ const App = () => {
     }
   };
 
+  const filteredNames = filterName === ""
+  ? persons
+  : persons.filter(persons => persons.name.toLowerCase() === filterName.toLowerCase())
+  
 
   const handleNameChange = (event) => {
     console.log(event.target.value);
@@ -37,6 +42,10 @@ const App = () => {
     console.log(event.target.value);
     setNewNumber(event.target.value);
   };
+  const handleFilterChange = (event) => {
+    console.log(event.target.value);
+    setFilterName(event.target.value);
+  };
 
   const Person = ({ persons }) => {
     return <li>{persons.name} {persons.number}</li>;
@@ -45,6 +54,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={filterName} onChange={handleFilterChange}></input>
+      </div>
+
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -58,7 +71,7 @@ const App = () => {
       <div>debug: {newName}</div>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((persons) => (
+        {filteredNames.map((persons) => (
           <Person key={persons.id} persons={persons}></Person>
         ))}
       </ul>
